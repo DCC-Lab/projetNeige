58667639af0b6e5e8f715e5031efa2c6a1850dcb
JLBegin <ludovick.begin@gmail.com>
2020-11-17 20:46:30 -0500

    Create nanoMockFeed.ino

---
+++ controller/nanoMockFeed/nanoMockFeed.ino
@@ -0,0 +1,12 @@
+// Simulate PD nano by sending an array of 4 values at max rate
+
+int c = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println(String(c) + ',' + String(c*2) + ',' + String(c*3) + ',' + String(c*4));
+  c = (c + 1) % 100;
+}
