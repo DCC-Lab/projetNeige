94bda01d1d997e8dab1dd9ceff728ec6e016983f
JLBegin <ludovick.begin@gmail.com>
2020-11-20 01:09:43 -0500

    assert 3G and internet connection and add Camera captures on intervals

--- controller/acquire.py
+++ controller/acquire.py
@@ -1,23 +1,48 @@
+import os
+import time
+import paramiko
+import numpy as np
+from serial import Serial
+from datetime import datetime
+from picamera import PiCamera
+from serial.tools import list_ports
+from http.client import HTTPConnection
+
+directory = os.path.dirname(os.path.abspath(__file__))
+
 """
 RaspBerry Pi Acquisition Script
 Launched at startup
 
 - Check for Git updates (?)
-- Wait for internet connection (?)
+- Activate 3G modem
+- Wait for internet connection
 - For each COM, integrate signal for N points. Each COM corresponds to 4 PD.
 - Save data to file
-- If time to take a picture (count or RTC check), take a CAM pic, compress it and save it.
-- Send generated data over SSH
+- Take picture if it's time to (interval check)
+- Send data and image to server over SSH
 """
 
-import os
-import paramiko
-import numpy as np
-from serial import Serial
-from datetime import datetime
-from serial.tools import list_ports
+N = 100
+captureIntervals = 6
 
-directory = os.path.dirname(os.path.abspath(__file__))
+
+def connectToInternet():
+    print("... Connecting.")
+    # Activate 3G modem (turns off if it lost power)
+    s = Serial("/dev/ttyUSB2", baudrate=115200)
+    s.write("""AT#ECM=1,0,"","",0\r""".encode())
+    s.close()
+    
+    # Simple HEAD request to test internet connection.
+    conn = HTTPConnection("www.google.com", timeout=10)
+    try:
+        conn.request("HEAD", "/")
+        conn.close()
+        return 1
+    except:
+        conn.close()
+        return 0
 
 
 def readData(ser, N):
@@ -34,6 +59,22 @@ def readData(ser, N):
     return data
 
 
+def captureRequired(interval: int = 6):
+    countPath = os.path.join(directory, "data/count.txt")
+    with open(countPath, "r") as f:
+        count = int(f.readlines()[0])
+    with open(countPath, "w+") as f:
+        f.write(str(count+1) + "\n")
+    if count % interval == 0:
+        return True
+    return False
+
+
+def capture(filepath):
+    camera = PiCamera()
+    camera.capture(filepath)
+
+
 # public 24.201.18.112, lan 192.168.0.188 
 def copyToServer(filepath, server="24.201.18.112", username="Alegria"):
     ssh = paramiko.SSHClient()
@@ -47,14 +88,17 @@ def copyToServer(filepath, server="24.201.18.112", username="Alegria"):
 
 
 if __name__ == "__main__":
+    recTimeStamp = datetime.now().strftime("%y%m%d_%H%M")
+    # ports = [e.device for e in list_ports.comports()]
+    # print("Available ports: ", ports)
     
-    # nanoPorts = [e.device for e in list_ports.comports()]
-    nanoPorts = ['/dev/ttyACM0']
-
-    nanoPorts *= 4  # duplicate single COM into 4 for testing
-    N = 100
-
+    r = connectToInternet()
+    print("... Internet is {}.".format(["DOWN", "UP"][r]))
+    
+    print("... Acquiring.")
     data = []
+    nanoPorts = ['/dev/ttyACM0']
+    nanoPorts *= 4  # duplicate single COM x4 for testing
     for port in nanoPorts:
         s = Serial(port, baudrate=115200)
         s.flushInput()
@@ -66,7 +110,17 @@ if __name__ == "__main__":
     data = np.concatenate(data, axis=1)  # shape (N, 16)
     data = np.mean(data, axis=0)  # shape (16,)
 
-    filepath = "data/PD_{}.txt".format(datetime.now().strftime("%y%m%d_%H%M"))
-    np.savetxt(os.path.join(directory, filepath), data)
+    dataFilePath = "data/PD_{}.txt".format(recTimeStamp)
+    np.savetxt(os.path.join(directory, dataFilePath), data)
+    print("... Saved to disk.")
 
-    copyToServer(filepath)
+    copyToServer(dataFilePath)
+    print("... Data saved to server.")
+    
+    if captureRequired(interval=captureIntervals):
+        imageFilePath = "data/image_{}.jpg".format(recTimeStamp)
+        capture(os.path.join(directory, imageFilePath))
+        copyToServer(imageFilePath)
+        print("... Image saved to server.")
+    
+    print("Acquisition successful. ")
