5715cc7043821f9832516c7c54f68ad6669fc7ae
JLBegin <ludovick.begin@gmail.com>
2020-11-21 21:37:23 -0500

    update mock feed with PCB ID

--- controller/nanoMockFeed/nanoMockFeed.ino
+++ controller/nanoMockFeed/nanoMockFeed.ino
@@ -1,12 +1,13 @@
 // Simulate PD nano by sending an array of 4 values at max rate
 
 int c = 0;
+int uniqueID = 2;
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  Serial.println(String(c) + ',' + String(c*2) + ',' + String(c*3) + ',' + String(c*4));
+  Serial.println(String(uniqueID) + ',' + String(c) + ',' + String(c*2) + ',' + String(c*3) + ',' + String(c*4));
   c = (c + 1) % 100;
 }
