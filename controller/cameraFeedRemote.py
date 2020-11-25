e5b001e5b20a69c5cfb90537ec57b77c0aa57455
JLBegin <ludovick.begin@gmail.com>
2020-11-25 00:00:50 -0500

    add cameraFeedRemote

---
+++ controller/cameraFeedRemote.py
@@ -0,0 +1,9 @@
+import subprocess
+from picamera import PiCamera
+
+cam = PiCamera()
+cam.resolution = (1024, 576)
+for _ in range(60):
+    cam.capture("feed.jpg")
+    subprocess.run(["scp", "feed.jpg", "Alegria@24.201.18.112:feed.jpg"])
+
