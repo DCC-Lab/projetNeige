4e26aa4e6fd57856bfcf4624de12b81d599034a8
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2021-04-11 13:28:15 -0400

    port correction

--- server/developmentServer/DatabaseConfigs.py
+++ server/developmentServer/DatabaseConfigs.py
@@ -1,7 +1,7 @@
 from DatabaseConfigModel import DatabaseConfigModel
 
 localhost_database_config = DatabaseConfigModel("localhost", 32000, "projetneigedb", "projetNeige", "pn2020")
-internal_databse_config = DatabaseConfigModel("192.168.2.11", 3200, "projetneigedb", "projetNeige", "pn2020")
+internal_databse_config = DatabaseConfigModel("192.168.2.11", 3306, "projetneigedb", "projetNeige", "pn2020")
 remote_database_config = DatabaseConfigModel("74.15.117.115", 3306, "projetneigedb", "projetNeige", "pn2020")
 
 
