d50ea2d45c0273b0c186e4b00aeaddbfdc0da320
Valerie Dionne <valerie.dionne.8@ulaval.ca>
2022-05-24 11:57:50 -0400

    update

--- heights/fusionnerfichiers.py
+++ heights/fusionnerfichiers.py
@@ -3,20 +3,37 @@ import os
 import glob
 from datetime import datetime as dt
 
+def Exceltocsv(path, name, headers, rows=None, colums=None,):
+    """read an Excel file and transform the useful data in a csv
+    
+    path: str of the path of the file
+    name: name of the csv file
+    headers: list of the names of the colums in the dataframe (must have the same number of colums entered)
+    rows: int of the number of the rows we want to store (all by default)
+    colums: str of the letter of the solums we want to store (all by default) -- EX: 'A,K' (colums A and K) 
+                                                                                     'A-K' (colums A to K)
+
+    return the data and save a csv
+    """
+    data = pd.read_excel(path, names=headers, usecols=colums, nrows=rows, na_values='na')
+    data = data.dropna()
+    data.to_csv(f"{name}.csv", index=False)
+    return data
+
 def joincsvfiles(path, common, newname):
     """join csv files with a similar name in alphabetic order
     
-    path: str of the path of the file
-    common: str of what is similar in all the files you want to join (at the beginning of the name)
+    path: str of the common path of the files
+    common: str of what is similar in all the files you want to join
     newname: name of the new file
 
-    return csv of the dataframe
+    return the dataframe and save a csv
     """
     #takes the files and put them in a list
-    files_joined = os.path.join(path, f"{common}*.csv")
+    files_joined = os.path.join(path, f"*{common}*.csv")
     list_files = glob.glob(files_joined)
     #join the files in a dataframe panda
-    dataFrame = pd.concat(map(pd.read_csv, list_files), ignore_index=True)
+    dataFrame = pd.concat(map(pd.read_csv, list_files))
     #save the dataframe in a csv
     dataFrame.to_csv(f"{newname}.csv", index=False)
     return dataFrame
@@ -39,7 +56,35 @@ def renamefiles_asDanwishes(path):
         if "2022" not in name:
             os.rename(nfc, os.path.join(path, f"{date}-" + name))
 
+def add_id(path, name_id, id, newname):
+    """add a colum of the same id to all the data of the file chosen
+    
+    path: str of the path of the file
+    name_id: name of the colum we add
+    id: str or value of the id
+    newname: name of the file modified
+    
+    return the dataframe and save csv
+    """
+    df = pd.read_csv(path, header=0)
+    rows = df.shape[0]
+    df[name_id] = [id for _ in range(rows)]
+    df.to_csv(f"{newname}.csv", index=False)
+    return df
+
+def stripcolums(path, newname, colum):
+    """delete colums of the file chosen
+    
+    path: str of the path of the file
+    newname: name of the file modified
+    colum: list of the lower and upper bound of the colums we want to keep
+
+    return the dataframe and save csv
+    """
+    df = pd.read_csv(path, header=0)
+    df = df.iloc[:, colum[0]:colum[1]]
+    df.to_csv(f"{newname}.csv", index=False)
+    return df
 
 #enter your path here
-path = ''
-renamefiles_asDanwishes(path)
+path = 'C:\\Users\\Proprio\\Documents\\UNI\\Stage\\Data\\all_heightsAM.csv'
