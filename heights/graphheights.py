9d390d7eef48530cc019ec324aa50340279a24d5
Valerie Dionne <valerie.dionne.8@ulaval.ca>
2022-06-20 16:43:43 -0400

    Update graphheights.py

--- heights/graphheights.py
+++ heights/graphheights.py
@@ -1,20 +1,22 @@
 import pandas as pd
+import plotly
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-def graphheight(path, color=None, realNames=None, constant=None):
+def graphheight(path, color=None, realNames=None, title=None, constant=None):
     """make a plotly scatter with a csv file
     
     path: str of the path of the file
     color: str of the colum of the file which separates the data
     realNames: dict of the data in the colum 'color' and the associated labels who will appear in the legend
+    title: str of the title of the graph
     constant: str of a caracteristic who is constant in this data (None by default)
 
     return the figure
     """
     data = pd.read_csv(path)
-    fig = px.scatter(data, x='date', y='irradiance_denoised', color=color, title=f"Height of the snow {constant+' ' if constant else ''}over time according to the {color} (2020-2021)") #, log_y=True
+    fig = px.scatter(data, x='date', y='irr', color=color, title=title) #, log_y=True // f"Height of the snow {constant+' ' if constant else ''}over time according to the {color} (2020-2021)"
     if realNames:
         for i, dict in enumerate(fig.data):
             for elem in dict:
@@ -35,9 +37,9 @@ def twograhs(path1, path2, title, yaxis, scale):
     """
     data1 = pd.read_csv(path1)
     data2 = pd.read_csv(path2)
-
-    trace1 = go.Scatter(x=data1['date'], y=data1[yaxis[0]], mode='markers', name=yaxis[0])
-    trace2 = go.Scatter(x=data2['date'], y=data2[yaxis[1]], yaxis="y2", mode='markers', name=yaxis[1])
+    cols = plotly.colors.qualitative.Pastel1
+    trace1 = go.Scatter(x=data1['date'], y=data1[yaxis[0]], mode='markers', name=yaxis[0], opacity=0.5)
+    trace2 = go.Scatter(x=data2['date'], y=data2[yaxis[1]], yaxis="y2", mode='markers', name=yaxis[1], opacity=0.5)
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(trace1)
@@ -50,22 +52,23 @@ def twograhs(path1, path2, title, yaxis, scale):
 
 
 #Write info here
-path1 = 'C:\\Users\\Proprio\\Documents\\UNI\\Stage\\Data\\400F325_norm1200-4.csv'
-path2 = 'C:\\Users\\Proprio\\Documents\\UNI\\Stage\\Data\\400F325_norm1200-4.csv'
+path1 = 'C:\\Users\\Proprio\\Documents\\UNI\\Stage\\Data\\path.csv'
+path2 = 'C:\\Users\\Proprio\\Documents\\UNI\\Stage\\Data\\400F1375.csv'
 # names = {'A': 'Automatic', 'C': 'CRN4', 'M': 'Manual', 'F':'Forent'}
 
-yaxis = ['irradiance self-norm', 'irradiance self-norm i0-norm_denoisedR std-norm']
-df = pd.read_csv(path1)
-ira = df[yaxis[1]]
-ref = df[yaxis[0]]
-indexref = df.loc[ref == max(ref)].index[0]
-max_ira = ira.loc[indexref]
-scale = [[0, max(ref)], [0, max_ira*1.5]]
+yaxis = ['irradiance', 'irradiance']
+# df = pd.read_csv(path1)
+# ira = df[yaxis[1]]
+# ref = df[yaxis[0]]
+# indexref = df.loc[ref == max(ref)].index[0]
+# max_ira = ira.loc[indexref]
+scale = [[-0.1, 2], [-0.1, 2]]
 
 #show and save figure
-fig = twograhs(path1, path2, '4-B', yaxis, scale)
-fig.show()
-fig.write_html('all_400F325_norm1200-4B.html')
-
-# fig = graphheight(path2)
+# fig = twograhs(path2, path1, '400F1375', yaxis, scale)
 # fig.show()
+# fig.write_html('all_400F325_norm1200-4B.html')
+
+fig = graphheight(path2, title='400F1375')
+fig.show()
+# fig.write_html('all_400F1000.html')
